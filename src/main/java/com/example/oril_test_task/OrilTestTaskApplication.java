4168240c606773f982602f5266e7fe616cd7c14f
kpavlunenko <kpavlunenko@gmail.com>
2022-03-14 16:05:54 +0200

    test task

---
+++ src/main/java/com/example/oril_test_task/OrilTestTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.oril_test_task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrilTestTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrilTestTaskApplication.class, args);
+    }
+
+}
